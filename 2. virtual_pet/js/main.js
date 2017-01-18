bb5d060d09de29d1c1d76d87a5f11d20bf937962
you <you@example.com>
2017-01-18 20:38:27 -0300

    init virtual pet

---
+++ 2. virtual_pet/js/main.js
@@ -0,0 +1,113 @@
+//this game will have only 1 state
+var GameState = {
+
+
+  init: function() {
+  	//scaling options
+  	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  	this.scale.pageAlignHorizontally = true;
+  	this.scale.pageAlignVertically = true;
+  },
+
+  preload: function() {
+    this.load.image('backyard', 'assets/images/backyard.png');    
+    this.load.image('apple', 'assets/images/apple.png');    
+    this.load.image('candy', 'assets/images/candy.png');    
+    this.load.image('rotate', 'assets/images/rotate.png');    
+    this.load.image('toy', 'assets/images/rubber_duck.png');    
+    this.load.image('arrow', 'assets/images/arrow.png');   
+    this.load.spritesheet('pet', 'assets/images/pet.png', 97, 83, 5, 1, 1); 
+  },
+
+  create: function() {      
+  	this.background = this.game.add.sprite(0, 0, 'backyard');
+
+  	this.pet = this.game.add.sprite(100, 400, 'pet');
+  	this.pet.anchor.setTo(0.5);
+
+  	//custom properties
+  	this.pet.customParams = {health: 100, fun: 100};
+      
+    this.pet.inputEnabled = true;
+    this.pet.input.enableDrag();
+
+  	this.apple = this.game.add.sprite(72, 570, 'apple');
+    this.apple.anchor.setTo(0.5);
+    this.apple.inputEnabled = true;
+    this.apple.customParams = {health: 20};
+    this.apple.events.onInputDown.add(this.pickItem, this);
+      
+  	this.candy = this.game.add.sprite(144, 570, 'candy');
+    this.candy.anchor.setTo(0.5);
+    this.candy.inputEnabled = true;
+    this.candy.customParams = {health: -10, fun: 10};
+    this.candy.events.onInputDown.add(this.pickItem, this);
+      
+  	this.toy = this.game.add.sprite(216, 570, 'toy');
+    this.toy.anchor.setTo(0.5);
+    this.toy.inputEnabled = true;
+    this.toy.customParams = {fun: 20};
+    this.toy.events.onInputDown.add(this.pickItem, this);
+      
+  	this.rotate = this.game.add.sprite(288, 570, 'rotate');
+    this.rotate.anchor.setTo(0.5);
+    this.rotate.inputEnabled = true;
+    this.rotate.events.onInputDown.add(this.rotatePet, this);
+      
+    this.buttons = [this.apple, this.candy, this.toy, this.rotate];
+    
+    // nothing is selected
+    this.selectedItem = null;
+    this.uiBlocked = false;
+
+  },
+    
+  pickItem: function(sprite, event) {
+    if (!this.uiBlocked) {
+        console.log("pick item");
+        
+        this.clearSelection();
+        
+        sprite.alpha = 0.4;
+        
+        this.selectedItem = sprite;
+    }
+  },
+    
+  clearSelection: function() {
+      this.buttons.forEach(function(element, index) {
+          element.alpha = 1;
+      });
+      
+      this.selectedItem = null;
+  },
+
+  rotatePet: function(sprite, event) {
+      if (!this.uiBlocked) {
+          console.log("rotating..");
+          
+          this.uiBlocked = true;
+          
+          this.clearSelection();
+          
+          sprite.alpha = 0.4;
+          
+          var petRotation = this.game.add.tween(this.pet);
+          petRotation.to({angle: '+720'}, 1000);
+          petRotation.onComplete.add(function() {
+              this.uiBlocked = false;
+              sprite.alpha = 1;
+              this.pet.customParams.fun += 10;
+              console.log(this.pet.customParams.fun);
+          }, this);
+          petRotation.start();
+      }
+  }
+  
+};
+
+//initiate the Phaser framework
+var game = new Phaser.Game(360, 640, Phaser.AUTO);
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
